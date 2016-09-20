33d3378aca64fa39de92f1cd481c3f83d344bb0e
Dan <dalessandro5138@gmail.com>
2016-09-19 21:33:13 -0400

    setup robot

---
+++ src/robots/AgentSmitty.java
@@ -0,0 +1,15 @@
+package robots;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class AgentSmitty extends Robot {
+
+    public void run(){
+
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e){
+
+    }
+}
